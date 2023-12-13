64055aa423f511d8dc001ba9af2395da91004516
Darkness <inflexibled@gmail.com>
2023-12-13 17:44:50 +0300

    Фиксы хентая и капибар

--- src/main/java/com/buddha/Main.java
+++ src/main/java/com/buddha/Main.java
@@ -55,6 +55,7 @@ public class Main extends ListenerAdapter {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("https://tenor.com/view/capybara-orange-spa-self-self-care-gif-15986252").queue();
         }
+
         if (event.getMessage().getContentRaw().equalsIgnoreCase("Цинцюэ имба")) {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("https://media.discordapp.net/attachments/1076507277793706005/1183740549593841704/IMG_20231206_201306.jpg?ex=65896f4b&is=6576fa4b&hm=49fe02853fadb8b6a09eeb72bfe1fdb98f8f13aab8340499bdd3ce00e1ba5dce&=&format=webp&width=551&height=671").queue();
@@ -64,18 +65,15 @@ public class Main extends ListenerAdapter {
             var elb = event.getMessage().getContentRaw().substring(6, event.getMessage().getContentRaw().length() - 1);
             event.getMember().modifyNickname(elb).queue();
         }
+
         if (event.getMessage().getContentRaw().equalsIgnoreCase("пивасик")) {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("https://media.discordapp.net/attachments/1025140219525206087/1183889904334282812/MrNDF2Ah5W8.jpg?format=webp&width=671&height=671").queue();
         }
-        if (event.getMessage().getContentRaw().equalsIgnoreCase("Цинцюэ имба")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("https://media.discordapp.net/attachments/1076507277793706005/1183740549593841704/IMG_20231206_201306.jpg?ex=65896f4b&is=6576fa4b&hm=49fe02853fadb8b6a09eeb72bfe1fdb98f8f13aab8340499bdd3ce00e1ba5dce&=&format=webp&width=551&height=671").queue();
-        }
 
         if (event.getMessage().getContentRaw().startsWith("Пикча ")) {
             var tag = event.getMessage().getContentRaw().substring("Пикча ".length());
-            var url = HentPoster.getImageUrl(tag);
+            var url = HentaiPoster.getImageUrl(tag);
 
             event.getMessage().reply(url).queue();
         }
